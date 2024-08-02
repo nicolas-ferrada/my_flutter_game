d56df7ec059f3be48f6e96e4eed3abe9832fcb1a
nicolasferrada <118396552+nicolas-ferrada@users.noreply.github.com>
2024-08-01 21:23:29 -0400

    feat: add my_game class to represent the game instance. Initialize it in main file.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,10 @@
+import 'package:flame/game.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() {}
+import 'my_game.dart';
+
+void main() {
+  final MyGame game = MyGame();
+  runApp(GameWidget(game: kDebugMode ? MyGame() : game));
+}
